e053bfae255ea5c0af50f4ea1959fb13e80663ff
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-14 03:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter((driver) => driver.revenue > revenue);
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map((driver) => driver.name);
+}
+
+function exactMatch(drivers, objectToMatch) {
+  return drivers.filter(drivers, (driver) => {
+    for (let property in objectToMatch) {
+      if (driver[property] === objectToMatch[property]) {
+        return true;
+      }
+    }
+    return false;
+  });
+}
+
+function exactMatchToList(drivers, objectToMatch) {
+  return exactMatch(drivers, objectToMatch).map((driver) => driver.name);
+}
